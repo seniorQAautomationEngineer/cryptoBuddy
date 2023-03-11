f0354d57afdd739b77c7d65aab1cde21a940c4e7
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-03-10 19:05:03 -0500

    Added search results table

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,47 @@ $.getJSON('./json/cryptoData.json', function(data) {
     $.each(socialNetworks, function(index, socialNetwork) {
     $('#socialNetworks').append('<option value="' +  socialNetwork.name + '">' +  socialNetwork.name + '</option>')
     });
+    var types = data.types;
+    $.each(types, function(index, type) {
+    $('#cryptoType').append('<option value="' +  type.name + '">' +  type.name + '</option>')
+    });
   });
+});
+
+
+
+document.querySelector('#searchForm').addEventListener('submit', function(event) {
+  // prevent the default form submission behavior
+  event.preventDefault();
+
+  // get the selected values from the form
+  const coin = document.querySelector('#coinList').value;
+  const country = document.querySelector('#countryList').value;
+  const socialNetwork = document.querySelector('#socialNetworks').value;
+  const cryptoType = document.querySelector('#cryptoType').value;
+
+  // fetch the data from the JSON file
+  fetch('json/searchResults.json')
+    .then(response => response.json())
+    .then(data => {
+      debugger;
+      // filter the data based on the selected values
+      const filteredData = data.coins.filter(item => {
+        return item.name === coin && item.groups.some(group => group.source === socialNetwork && group.type === cryptoType);
+      });
+
+    // build the HTML for the search results table
+      let html = '';
+      filteredData.forEach(item => {
+        const name = item.name;
+        item.groups.forEach(group => {
+          const description = group.description;
+          const link = group.link;
+          const source = group.source;
+          const type = group.type;
+          html += `<tr><td>${name}</td><td>${description}</td><td>${source}</td><td>${type}</td><td><a href="${link}" target="_blank">${link}</a></td></tr>`;
+        });
+      });
+    })
+    .catch(error => console.error(error));
 });
